63333770d980a64b5575339f8602bb4c08b17b74
dorian marechal <codinglabs@Codinglabs-2.local>
2024-07-23 22:27:23 +0200

    ajout des balise meta

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,8 +12,8 @@ const Home = () => {
         <div className="text">
             <div className="bio">
                 <h1>Bonjour, je m'appel Dorian 👋</h1>
-                <p>Je suis un développeur full stack (PHP, Symfony, React.js & MySQL) spécialisé dans la conception et la création d'application,
-                accessibles, visuellement attrayants et réactifs. Je viens d'obtenir mon titre professionnel de Concepteur Développeur d'Applications.
+                <p>Je suis un développeur full stack (PHP, Symfony, React.js & MySQL) spécialisé dans la conception et la création d'applications,
+                Accessibles, visuellement attrayants et réactifs. Je viens d'obtenir mon titre professionnel de Concepteur Développeur d'Applications.
                 Même après plus de 3 ans à créer des applications web, j'aborde chaque projet avec la même passion qu'au premier jour.</p>
             </div>
             <div className="location-availability">
